c0d21379d1f32ed3339d4e00134145389e5de190
akaiDing <tingtind@umich.edu>
2023-09-18 22:21:09 -0400

    modify restrict

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=dinov99
 #SBATCH --partition=spgpu
 #SBATCH --gpus=2
-#SBATCH --mem-per-cpu=60g
+#SBATCH --mem-per-cpu=20g
 #SBATCH --mail-user=tingtind@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 module purge
@@ -11,7 +11,8 @@ module load gcc cuda/11.7.1 cudnn/11.7-v8.7.0 python
 # source /nfs/turbo/umms-dinov/LLaMA/2.0.0/bin/activate
 source env/bin/activate
 
-export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:2048' # for 44GB GPU
+# for 44GB GPU, restrict the allocation to 5 GB
+export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:5'
 # llama2 7b text completion model
 # torchrun --nproc_per_node 1 /home/tingtind/llama/example_text_completion.py \
 #     --ckpt_dir /nfs/turbo/umms-dinov/LLaMA/2.0.0/llama/modeltoken/llama-2-7b \
