377b1806f1f68b1f774fb8698c92967b9d4c26e4
akaiDing <tingtind@umich.edu>
2023-09-20 21:34:33 -0400

    try test hf

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=test
 #SBATCH --account=dinov99
 #SBATCH --partition=spgpu
-#SBATCH --gpus=3
-#SBATCH --mem-per-cpu=50g
+#SBATCH --gpus=2
+#SBATCH --mem-per-cpu=20g
 #SBATCH --mail-user=tingtind@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 module purge
@@ -12,10 +12,10 @@ module load gcc cuda/11.7.1 cudnn/11.7-v8.7.0 python
 source env/bin/activate
 
 # for 44GB GPU, restrict the allocation to 5 GB
-export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:1024'
+# export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:1024'
 # llama2 7b text completion model
 # torchrun --nproc_per_node 1 /home/tingtind/llama/example_text_completion.py \
 #     --ckpt_dir /nfs/turbo/umms-dinov/LLaMA/2.0.0/llama/modeltoken/llama-2-7b \
 #     --tokenizer_path /nfs/turbo/umms-dinov/LLaMA/1.0.1/llama/modeltoken/tokenizer.model
 
-python finetune_peft.py
+python test_hg.py
