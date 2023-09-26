bd798035fd1d7cc80741e8fb84795ec4a7682d89
akaiDing <tingtind@umich.edu>
2023-09-25 20:54:24 -0400

    add test dataset

---
+++ test_loaddataset.py
@@ -0,0 +1,31 @@
+
+
+# !pip install -q accelerate==0.21.0 peft==0.4.0 bitsandbytes==0.40.2 transformers==4.31.0 trl==0.4.7
+
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+# The model that you want to train from the Hugging Face hub
+model_name = "NousResearch/Llama-2-7b-chat-hf"
+
+# The instruction dataset to use
+dataset_name = "bigbio/med_qa"
+
+# Fine-tuned model name
+new_model = "llama-2-7b-miniguanaco"
+
+# Load dataset (you can process it here)
+dataset = load_dataset(dataset_name, split="train")
+
