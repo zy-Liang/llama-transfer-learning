792cf61c8ed953ec731e06954b7b0ce39c858d64
akaiDing <tingtind@umich.edu>
2023-09-26 12:25:25 -0400

    modify test script

--- test.sh
+++ test.sh
@@ -6,17 +6,12 @@
 #SBATCH --mem-per-cpu=20g
 #SBATCH --mail-user=tingtind@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=01-00:00:00
 module purge
 module load gcc cuda/11.7.1 cudnn/11.7-v8.7.0 python
 # source /nfs/turbo/umms-dinov/LLaMA/2.0.0/bin/activate
 source env/bin/activate
 
-# for 44GB GPU, restrict the allocation to 5 GB
-# export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:1024'
-# llama2 7b text completion model
-# torchrun --nproc_per_node 1 /home/tingtind/llama/example_text_completion.py \
-#     --ckpt_dir /nfs/turbo/umms-dinov/LLaMA/2.0.0/llama/modeltoken/llama-2-7b \
-#     --tokenizer_path /nfs/turbo/umms-dinov/LLaMA/1.0.1/llama/modeltoken/tokenizer.model
 
 export 'HUGGINGFACE_TOKEN=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'
 
