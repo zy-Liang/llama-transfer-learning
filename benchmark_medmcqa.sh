9e7160fe1b5a2ccb51cb7503a63b442ec291b00e
akaiDing <tingtind@umich.edu>
2023-10-12 12:00:06 -0400

    add script

---
+++ benchmark_medmcqa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --account=dinov99
+#SBATCH --partition=spgpu
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=20g
+#SBATCH --mail-user=tingtind@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=01-00:00:00
+module purge
+module load gcc cuda/11.7.1 cudnn/11.7-v8.7.0 python
+source env/bin/activate
+
+
+export 'HUGGINGFACE_TOKEN=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'
+
+huggingface-cli login --token $HUGGINGFACE_TOKEN
+
+python benchmark_medmcqa.py
