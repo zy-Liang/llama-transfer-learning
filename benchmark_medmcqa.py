600b1268437851ce6164ae2d38c0753423287ca5
akaiDing <tingtind@umich.edu>
2023-10-12 11:59:39 -0400

    test medmcqa

--- benchmark_medmcqa.py
+++ benchmark_medmcqa.py
@@ -46,39 +46,39 @@ bnb_config = BitsAndBytesConfig(
 )
 
 # Load base model
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     quantization_config=bnb_config,
-#     device_map=device_map
-# )
-
-# # Load LLaMA tokenizer
-# tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-# tokenizer.pad_token = tokenizer.eos_token
-# tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
-
-# Fine-tuned model name
-new_model = "llama-2-7b-medmcqa"
-
-model_name = "akaiDing/llama-2-7b-med_qa"
-
-device_map = {"": 0}
-
-base_model = AutoModelForCausalLM.from_pretrained(
+model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    low_cpu_mem_usage=False,
-    return_dict=True,
-    torch_dtype=torch.float16,
-    device_map=device_map,
     quantization_config=bnb_config,
+    device_map=device_map
 )
-model = PeftModel.from_pretrained(base_model, new_model)
-model = model.merge_and_unload()
 
-# Reload tokenizer to save it
+# Load LLaMA tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
+tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+
+# Fine-tuned model name
+# new_model = "llama-2-7b-medmcqa"
+
+# model_name = "akaiDing/llama-2-7b-med_qa"
+
+# device_map = {"": 0}
+
+# base_model = AutoModelForCausalLM.from_pretrained(
+#     model_name,
+#     low_cpu_mem_usage=False,
+#     return_dict=True,
+#     torch_dtype=torch.float16,
+#     device_map=device_map,
+#     quantization_config=bnb_config,
+# )
+# model = PeftModel.from_pretrained(base_model, new_model)
+# model = model.merge_and_unload()
+
+# # Reload tokenizer to save it
+# tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+# tokenizer.pad_token = tokenizer.eos_token
+# tokenizer.padding_side = "right"
 
 # model.push_to_hub(new_model, use_temp_dir=False)
 # tokenizer.push_to_hub(new_model, use_temp_dir=False)
