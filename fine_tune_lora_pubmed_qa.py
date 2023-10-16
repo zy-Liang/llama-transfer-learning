8be814db7f354eb5adad80f594d63afc3e28ea98
akaiDing <tingtind@umich.edu>
2023-10-16 14:49:22 -0400

    add finetune for pubmed

--- fine_tune_lora_pubmed_qa.py
+++ fine_tune_lora_pubmed_qa.py
@@ -23,7 +23,7 @@ from peft import LoraConfig, PeftModel
 from trl import SFTTrainer
 
 # The model that you want to train from the Hugging Face hub
-model_name = "llama-2-7b-medmcqa"
+model_name = "akaiDing/llama-2-7b-medmcqa"
 
 # The instruction dataset to use
 dataset_name = "pubmed_qa"
@@ -133,147 +133,141 @@ device_map = {"": 0}
 # Load dataset (you can process it here)
 dataset = load_dataset(dataset_name, 'pqa_artificial', split="train")
 
-print(len(dataset))
 
-# # Load tokenizer and model with QLoRA configuration
-# compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+# Load tokenizer and model with QLoRA configuration
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
 
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=use_4bit,
-#     bnb_4bit_quant_type=bnb_4bit_quant_type,
-#     bnb_4bit_compute_dtype=compute_dtype,
-#     bnb_4bit_use_double_quant=use_nested_quant,
-# )
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
 
-# # Check GPU compatibility with bfloat16
-# if compute_dtype == torch.float16 and use_4bit:
-#     major, _ = torch.cuda.get_device_capability()
-#     if major >= 8:
-#         print("=" * 80)
-#         print("Your GPU supports bfloat16: accelerate training with bf16=True")
-#         print("=" * 80)
+# Check GPU compatibility with bfloat16
+if compute_dtype == torch.float16 and use_4bit:
+    major, _ = torch.cuda.get_device_capability()
+    if major >= 8:
+        print("=" * 80)
+        print("Your GPU supports bfloat16: accelerate training with bf16=True")
+        print("=" * 80)
 
-# # Load base model
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     quantization_config=bnb_config,
-#     device_map=device_map
-# )
-# model.config.use_cache = False
-# model.config.pretraining_tp = 1
+# Load base model
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
 
-# # Load LLaMA tokenizer
-# tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-# tokenizer.pad_token = tokenizer.eos_token
-# tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+# Load LLaMA tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
 
-# # Load LoRA configuration
-# peft_config = LoraConfig(
-#     lora_alpha=lora_alpha,
-#     lora_dropout=lora_dropout,
-#     r=lora_r,
-#     bias="none",
-#     task_type="CAUSAL_LM",
-# )
+# Load LoRA configuration
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
 
-# # Set training parameters
-# training_arguments = TrainingArguments(
-#     output_dir=output_dir,
-#     num_train_epochs=num_train_epochs,
-#     per_device_train_batch_size=per_device_train_batch_size,
-#     gradient_accumulation_steps=gradient_accumulation_steps,
-#     optim=optim,
-#     save_steps=save_steps,
-#     logging_steps=logging_steps,
-#     learning_rate=learning_rate,
-#     weight_decay=weight_decay,
-#     fp16=fp16,
-#     bf16=bf16,
-#     max_grad_norm=max_grad_norm,
-#     max_steps=max_steps,
-#     warmup_ratio=warmup_ratio,
-#     group_by_length=group_by_length,
-#     lr_scheduler_type=lr_scheduler_type
-# )
+# Set training parameters
+training_arguments = TrainingArguments(
+    output_dir=output_dir,
+    num_train_epochs=num_train_epochs,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    optim=optim,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+    learning_rate=learning_rate,
+    weight_decay=weight_decay,
+    fp16=fp16,
+    bf16=bf16,
+    max_grad_norm=max_grad_norm,
+    max_steps=max_steps,
+    warmup_ratio=warmup_ratio,
+    group_by_length=group_by_length,
+    lr_scheduler_type=lr_scheduler_type
+)
 
-# def formatting_func(example):
-#     output_texts = []
-#     answers_index = ["A", "B", "C", "D"]
-#     for i in range(len(example['question'])):
-#         answers = [example['opa'][i], example['opb'][i], example['opc'][i], example['opd'][i]]
-#         choice_text = f"A. {example['opa'][i]}\nB. {example['opb'][i]}\nC. {example['opc'][i]}\nD. {example['opd'][i]}\n"
-#         text = (
-#             f"### Question: {example['question'][i]}\n"
-#             f"### Choices: {choice_text}\n"
-#             f"### Answer: {answers_index[int(example['cop'][i])]}. {answers[int(example['cop'][i])]}\n"
-#         )
-#         output_texts.append(text)
-#     return output_texts
+def formatting_func(example):
+    output_texts = []
+    for i in range(len(example['context'])):
+        text = (
+            f"### Question: {example['context'][i]['contexts'][0]}\n"
+        )
+        output_texts.append(text)
+    return output_texts
 
-# # Set supervised fine-tuning parameters
-# trainer = SFTTrainer(
-#     model=model,
-#     train_dataset=dataset,
-#     peft_config=peft_config,
-#     formatting_func=formatting_func,
-#     max_seq_length=max_seq_length,
-#     tokenizer=tokenizer,
-#     args=training_arguments,
-#     packing=packing,
-# )
+# Set supervised fine-tuning parameters
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset,
+    peft_config=peft_config,
+    formatting_func=formatting_func,
+    max_seq_length=max_seq_length,
+    tokenizer=tokenizer,
+    args=training_arguments,
+    packing=packing,
+)
 
-# # Train model
-# trainer.train()
+# Train model
+trainer.train()
 
-# # Save trained model
-# trainer.model.save_pretrained(new_model)
+# Save trained model
+trainer.model.save_pretrained(new_model)
 
-# # %load_ext tensorboard
-# # %tensorboard --logdir results/runs
+# %load_ext tensorboard
+# %tensorboard --logdir results/runs
 
-# # Ignore warnings
-# logging.set_verbosity(logging.CRITICAL)
+# Ignore warnings
+logging.set_verbosity(logging.CRITICAL)
 
-# # dataset = load_dataset(dataset_name, split="test")
+# dataset = load_dataset(dataset_name, split="test")
 
-# # Run text generation pipeline with our next model
-# # prompt = "What is a large language model?"
-# # pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
-# # result = pipe(f"<s>[INST] {prompt} [/INST]")
-# # print(result[0]['generated_text'])
+# Run text generation pipeline with our next model
+# prompt = "What is a large language model?"
+# pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
+# result = pipe(f"<s>[INST] {prompt} [/INST]")
+# print(result[0]['generated_text'])
 
-# # correct = 0
-# # for entry in dataset:
-# #     prompt = ""
-# #     pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=1)
-# #     result = pipe(f"<s>[INST] {prompt} [/INST]")
-# #     if result[0]['generated_text'] == answer:
-# #         correct += 1
-# # print(f"Test accuracy: {collect / len(dataset)}")
+# correct = 0
+# for entry in dataset:
+#     prompt = ""
+#     pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=1)
+#     result = pipe(f"<s>[INST] {prompt} [/INST]")
+#     if result[0]['generated_text'] == answer:
+#         correct += 1
+# print(f"Test accuracy: {collect / len(dataset)}")
 
-# # Empty VRAM
-# del model
-# # del pipe
-# del trainer
-# import gc
-# gc.collect()
-# gc.collect()
+# Empty VRAM
+del model
+# del pipe
+del trainer
+import gc
+gc.collect()
+gc.collect()
 
-# # Reload model in FP16 and merge it with LoRA weights
-# base_model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     low_cpu_mem_usage=True,
-#     return_dict=True,
-#     torch_dtype=torch.float16,
-#     device_map=device_map,
-# )
-# model = PeftModel.from_pretrained(base_model, new_model)
-# model = model.merge_and_unload()
+# Reload model in FP16 and merge it with LoRA weights
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    device_map=device_map,
+)
+model = PeftModel.from_pretrained(base_model, new_model)
+model = model.merge_and_unload()
 
-# # Reload tokenizer to save it
-# tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-# tokenizer.pad_token = tokenizer.eos_token
-# tokenizer.padding_side = "right"
+# Reload tokenizer to save it
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
 
-# model.push_to_hub(new_model, use_temp_dir=False)
-# tokenizer.push_to_hub(new_model, use_temp_dir=False)
+model.push_to_hub(new_model, use_temp_dir=False)
+tokenizer.push_to_hub(new_model, use_temp_dir=False)
