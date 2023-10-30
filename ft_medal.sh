4865ff8ff325a2f192d1898f77e7c564b9374867
akaiDing <tingtind@umich.edu>
2023-10-30 19:51:45 -0400

    dataset medal

---
+++ ft_medal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --account=dinov99
+#SBATCH --partition=spgpu
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=20g
+#SBATCH --mail-user=tingtind@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=05-00:00:00
+module purge
+module load gcc cuda/11.7.1 cudnn/11.7-v8.7.0 python
+source env/bin/activate
+
+
+export 'HUGGINGFACE_TOKEN=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'
+
+huggingface-cli login --token $HUGGINGFACE_TOKEN
+
+python fine_tune_lora_medal.py
